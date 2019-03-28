829f3282dabac359fa51bc3d3b4d668e4765241c
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-03-28 22:48:31 +0000

    Return partner information when fetchign synapses

--- R/synapses.R
+++ R/synapses.R
@@ -8,7 +8,8 @@
 #' @param conn optional, a neuprintr connection object, which also specifies the neuPrint server see \code{?neuprint_login}.
 #' If NULL, your defaults set in your R.profile or R.environ are used.
 #' @param ... methods passed to \code{neuprint_login}
-#' @return a n x n matrix, where the rows are input neurons and the columns are their targets
+#' @return a data frame, where each entry is a connection between the specified bodyid and its partner, either presynaptic to the bodyid (prepost=0) or postsynaptic (prepost=1).
+#' Each connection is associated with a synapse that has its own unique connector_id, XYZ coordinates and a confidence in its existence.
 #' @seealso \code{\link{neuprint_fetch_custom}}, \code{\link{neuprint_simple_connectivity}}, \code{\link{neuprint_common_connectivity}}
 #' @export
 #' @rdname neuprint_get_synapses
@@ -26,7 +27,7 @@ neuprint_get_synapses <- function(bodyid = bodyid, dataset = NULL, roi = "all", 
     }
     roi = sprintf("WHERE (exists(s.%s))", roi)
   }
-  cypher = sprintf("MATCH (a:`%s-Neuron`{bodyId:%s})<-[:To]-(c:ConnectionSet), (c)-[:Contains]->(s:Synapse) %s RETURN id(s) AS connector_id, s.type AS prepost, s.location.x AS x ,s.location.y AS y, s.location.z AS z, s.confidence AS confidence",
+  cypher = sprintf("MATCH (a:`%s-Neuron`{bodyId:%s})<-[:To]-(c:ConnectionSet), (c)-[:Contains]->(s:Synapse) %s RETURN id(s) AS connector_id, s.type AS prepost, s.location.x AS x ,s.location.y AS y, s.location.z AS z, s.confidence AS confidence, c.datasetBodyIds AS datasetBodyIds, c.timeStamp AS timestamp",
                    dataset,
                    bodyid,
                    roi)
@@ -36,5 +37,7 @@ neuprint_get_synapses <- function(bodyid = bodyid, dataset = NULL, roi = "all", 
   m = as.data.frame(m)
   m$prepost = ifelse(m$prepost=="post",1,0)
   m$bodyid = bodyid
+  m$partner = sapply(m$datasetBodyIds, function(i) unlist(strsplit(i,":"))[2])
+  m = m[,c("connector_id", "prepost", "x", "y", "z", "confidence", "bodyid", "partner", "timestamp")]
   m
 }
