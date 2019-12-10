23dc5683d8644d176e5c55a94ff5e7e9c7854d54
Gregory Jefferis <jefferis@gmail.com>
2019-12-10 19:47:23 +0000

    doc tweaks

--- R/info.R
+++ R/info.R
@@ -33,14 +33,17 @@ neuprint_version <- function(conn = NULL, ...){
 
 #' @title Get a vector of all the ROIs in a specified dataset
 #'
-#' @description  Get summary information about the datasets hosted by the neuPrint server in which you are interested
-#' @param dataset optional, a dataset you want to query. If NULL, the default specified by your R environ file is used. See \code{neuprint_login} for details.
-#' @param conn optional, a neuprintr connection object, which also specifies the neuPrint server see \code{?neuprint_login}.
-#' If NULL, your defaults set in your R.profile or R.environ are used.
+#' @description  Get summary information about the datasets hosted by the
+#'   neuPrint server in which you are interested
+#' @param dataset optional, a dataset you want to query. If NULL, the default
+#'   specified by your R environ file is used. See \code{neuprint_login} for
+#'   details.
+#' @param conn optional, a neuprintr connection object, which also specifies the
+#'   neuPrint server see \code{\link{neuprint_login}}. If NULL, your defaults
+#'   set in your R.profile or R.environ are used.
 #' @param ... methods passed to \code{neuprint_login}
 #' @seealso \code{\link{neuprint_login}}, \code{\link{neuprint_datasets}}
 #' @export
-#' @rdname neuprint_ROIs
 neuprint_ROIs <- function(dataset = NULL, conn = NULL, ...){
   if(is.null(dataset)){ # Get a default dataset if none specified
     dataset = unlist(getenvoroption("dataset"))
