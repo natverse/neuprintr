b98e0e48940d3c89a7b43df970a4a7fe3e282ca1
Gregory Jefferis <jefferis@gmail.com>
2019-12-21 01:08:33 +0000

    Fix bug when server has trailing slash
    * neuprint cannot handle a double slash in a path

--- R/fetch.R
+++ R/fetch.R
@@ -3,12 +3,14 @@ neuprint_fetch <- function(path, body = NULL, conn = NULL, parse.json = TRUE,
                            include_headers = TRUE, simplifyVector = FALSE, ...){
   path = gsub("\\/$|^\\/","",path)
   conn = neuprint_login(conn)
+  # you cannot have double / in any part of path
+  server = sub("\\/$", "", conn$server)
   req <-
     if (is.null(body)) {
-      httr::GET(url = file.path(conn$server, path, fsep = "/"),
+      httr::GET(url = file.path(server, path, fsep = "/"),
                 config = conn$config,  ...)
     }else {
-      httr::POST(url = file.path(conn$server, path, fsep = "/"),
+      httr::POST(url = file.path(server, path, fsep = "/"),
            body = body, config = conn$config, ...)
     }
   httr::stop_for_status(req)
