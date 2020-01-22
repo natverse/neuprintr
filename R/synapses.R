8a49b68fdfb2fe0e98f28a0b971d8305160d1fe7
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-01-22 19:17:09 +0000

    Remove timestamp column query

--- R/synapses.R
+++ R/synapses.R
@@ -84,7 +84,7 @@ neuprint_get_synapses <- function(bodyids, roi = NULL, progress = FALSE, dataset
   m$prepost = ifelse(m$prepost=="post",1,0)
  # m$bodyid = sapply(m$datasetBodyIds, function(i) unlist(strsplit(i,":"))[3])
  #  m$partner = sapply(m$datasetBodyIds, function(i) unlist(strsplit(i,":"))[2])
-  m = m[,c("connector_id", "prepost", "x", "y", "z", "confidence", "bodyid", "partner", "timestamp")]
+  m = m[,c("connector_id", "prepost", "x", "y", "z", "confidence", "bodyid", "partner")]
   m = subset(m, bodyid!=partner) # Automatically remove autapses, hopefully we only need to do this temporarily
   m
 }
