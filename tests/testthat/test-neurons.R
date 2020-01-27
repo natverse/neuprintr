36a32210fe170b083dbe5e602239d8c59915a506
Gregory Jefferis <jefferis@gmail.com>
2020-01-27 19:57:36 +0000

    Add basic test for neuprint_read_neurons

--- tests/testthat/test-neurons.R
+++ tests/testthat/test-neurons.R
@@ -4,3 +4,8 @@ test_that("neuprint_read_neuron_simple works", {
   expect_is(neuprint_read_neuron_simple(1700946301), 'neuron')
   expect_is(neuprint_read_neuron_simple(c(1700946301, 1700937093)), 'neuronlist')
 })
+
+test_that("neuprint_read_neuron_simple works", {
+  expect_is(neuprint_read_neurons(c(1700946301, 1700937093), heal = FALSE),
+            'neuronlist')
+})
