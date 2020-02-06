203edd367960b93530a96a9d20bf56871f2746a1
Gregory Jefferis <jefferis@gmail.com>
2020-02-06 20:30:49 +0000

    Reference login from package docs
    * also with passing mention of dataset specification

--- R/neuprintr-package.R
+++ R/neuprintr-package.R
@@ -1,3 +1,7 @@
+#' @section Login: See \code{\link{neuprint_login}} or the package README for
+#'   details of the how to login to a neuPrint server, the recommended initial
+#'   setup and (optionally) how to specify a default dataset.
+#'
 #' @section Online Documentation: See
 #'   \url{https://natverse.github.io/neuprintr/} for structured online
 #'   documentation.
