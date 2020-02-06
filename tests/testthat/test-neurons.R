b352ae6aeeb013a6834fd72bca463c50bfe29b4a
alexanderbates <alexander.shakeel.bates@gmail.com>
2020-02-06 20:45:01 +0000

    Update test-neurons.R

--- tests/testthat/test-neurons.R
+++ tests/testthat/test-neurons.R
@@ -1,7 +1,7 @@
 skip_if(as.logical(Sys.getenv("SKIP_NP_SERVER_TESTS")))
 
 test_that("neuprint_read_skeletons works", {
-  expect_is(neuprint_read_skeletons(c(1700946301, 1700937093)), 'neuronlist')
+  expect_is(neuprint_read_skeletons(c(1700946301, 1700937093), heal = FALSE), 'neuronlist')
 })
 
 test_that("neuprint_read_neurons works", {
