26d6c001473ff9abd38b239d3d86821f42abd884
Gregory Jefferis <jefferis@gmail.com>
2020-02-20 23:49:37 +0000

    Fix test for query with single quote

--- tests/testthat/test-name.R
+++ tests/testthat/test-name.R
@@ -7,7 +7,8 @@ test_that("test name searches ", {
   expect_is(penAbis <- neuprint_search("PEN_a(",field="type",fixed=TRUE),
             'data.frame')
   expect_equal(penAs,penAbis)
-  expect_is(mbon <-  neuprint_search("MBON16-like(a'3a)_R",field="name"), 'data.frame')
+  expect_is(mbon <- neuprint_search("(a'3a)", field="name", fixed=T),
+                                     'data.frame')
   expect_true(all(
     neuprint_search(
       penAbis$type[1],
