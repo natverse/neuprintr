b77d1c589508aa199e5b2a90d2f7a4dddd5181ad
Gregory Jefferis <jefferis@gmail.com>
2020-02-22 14:40:04 +0000

    basic tests for neuprint_find_neurons

--- tests/testthat/test-roi.R
+++ tests/testthat/test-roi.R
@@ -3,4 +3,8 @@ skip_if(as.logical(Sys.getenv("SKIP_NP_SERVER_TESTS")))
 test_that("neuprint_bodies_in_ROI works", {
   expect_is(df <- neuprint_bodies_in_ROI('ATL(L)'), 'data.frame')
   expect_named(df, c("bodyid", "voxels", "pre", "post", "roipre", "roipost"))
+
+  expect_is(df2 <- neuprint_find_neurons(
+    input_ROIs = "AL(R)", output_ROIs = "LH(R)"),
+    'data.frame')
 })
