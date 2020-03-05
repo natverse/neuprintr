605d28328abf0819a510ceeb59abc544301601e8
Gregory Jefferis <jefferis@gmail.com>
2020-03-05 23:31:28 +0000

    Option to chunk neuprint_get_synapses
    * also tweak progress settings

--- R/synapses.R
+++ R/synapses.R
@@ -12,8 +12,13 @@
 #' @param remove.autapses Whether to remove autaptic connections (default TRUE)
 #'   from the results. It appears that there is a relatively high number of
 #'   false positive autapses.
+#' @param chunk A logical specifying whether to split the query into multiple
+#'   chunks or an integer specifiying the size of those chunks (which defaults
+#'   to 20 when \code{chunk=TRUE}).
 #' @param progress if TRUE, a progress bar will be shown. This may slow the data
-#'   fetching process.
+#'   fetching process for smaller queries (<100 body ids). The default of
+#'   \code{progress=NULL} will only show a progress bar if the query will be
+#'   split into multiple chunks based on the \code{chunk} argument.
 #' @return a data frame, where each entry is a connection between the specified
 #'   bodyid and its partner, either presynaptic to the bodyid (prepost=0) or
 #'   postsynaptic (prepost=1). Each connection is associated with a synapse that
@@ -28,8 +33,8 @@
 #' \donttest{
 #' neuprint_get_synapses(c(818983130, 1796818119))
 #' }
-neuprint_get_synapses <- function(bodyids, roi = NULL, progress = FALSE,
-                                  remove.autapses=TRUE,
+neuprint_get_synapses <- function(bodyids, roi = NULL, remove.autapses=TRUE,
+                                  chunk=TRUE, progress = NULL,
                                   dataset = NULL, conn = NULL, ...){
   conn = neuprint_login(conn)
   bodyids = neuprint_ids(bodyids, conn = conn, dataset = dataset)
@@ -46,9 +51,25 @@ neuprint_get_synapses <- function(bodyids, roi = NULL, progress = FALSE,
     }
     roi = sprintf("AND (exists(s.`%s`))", roi)
   }
-  if(progress){
-    d  = do.call(rbind, pbapply::pblapply(bodyids,
-                                          function(bi) tryCatch(neuprint_get_synapses(
+  n <- length(bodyids)
+  if(is.numeric(chunk)) {
+    chunksize=chunk
+  } else {
+    # make smaller chunks when progress=T and there aren't so many bodyids
+    if(isTRUE(progress))
+      chunksize=min(20L, ceiling(n/10))
+    else
+      chunksize=20L
+  }
+
+  if(n>chunksize) {
+    nchunks=ceiling(n/chunksize)
+    chunks=rep(seq_len(nchunks), rep(chunksize, nchunks))[seq_len(n)]
+    bodyids <- split(bodyids, chunks)
+    # if we got here and progess is unset then set it
+    if(is.null(progress) || is.na(progress)) progress=TRUE
+    MYPLY <- if(isTRUE(progress)) pbapply::pblapply else lapply
+    d  = do.call(rbind, MYPLY(bodyids, function(bi) tryCatch(neuprint_get_synapses(
       bodyids = bi,
       roi = roi,
       progress = FALSE,
@@ -62,7 +83,6 @@ neuprint_get_synapses <- function(bodyids, roi = NULL, progress = FALSE,
       d=d[order(d$prepost, decreasing = TRUE),]
       rownames(d)=NULL
     }
-
 
     return(d)
   }
