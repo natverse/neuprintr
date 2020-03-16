80eb6558ab7d10600bf0f5dfeecebf29647f245e
Romain Franconville <romain.franconville@protonmail.com>
2020-03-16 14:52:15 -0400

    Update doc

--- R/connectivity.R
+++ R/connectivity.R
@@ -347,6 +347,10 @@ neuprint_simple_connectivity <- function(bodyids,
 #' @examples
 #' \donttest{
 #' neuprint_get_paths(c(1128092885,481121605),5813041365, n=c(1,2), weightT=20)
+#'
+#' neuprint_get_paths(c(1128092885,481121605),5813041365, n=c(1,2), weightT=20,by.roi=TRUE)
+#'
+#' neuprint_get_paths(c(1128092885,481121605),5813041365, n=c(1,2), weightT=20,roi=c("FB","LAL(-GA)(R)"))
 #' }
 neuprint_get_paths <- function(body_pre, body_post, n, weightT=5, roi=NULL, by.roi=FALSE,exclude.loops=TRUE,
                                dataset = NULL, conn = NULL, all_segments=FALSE, ...){
