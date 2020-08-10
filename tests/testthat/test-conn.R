7d2e386fa9734022557a8c5cd25032a75f49fd68
Gregory Jefferis <jefferis@gmail.com>
2020-08-10 02:11:54 +0100

    WIP: use server URL returned at login
    * should handle cases like http/https mismatch

--- tests/testthat/test-conn.R
+++ tests/testthat/test-conn.R
@@ -4,9 +4,12 @@ test_that("neuprint_login works", {
   expect_error(neuprint_login(server="https://neuprint.janelia.org", dataset='rhubarb'))
 })
 
+skip("don't test bad connection specs")
 test_that("neuprint_connection works", {
   expect_warning(expect_is(
-    neuprint_connection(server = "neuprint.janelia.org", dataset = 'rhubarb'),
+    neuprint_login(server = "neuprint.janelia.org",
+                   dataset = 'hemibrain:v1.0.1',
+                   Cache = F, Force = T),
     "neuprint_connection"
   ),
   regexp = "https://neuprint.janelia.org")
