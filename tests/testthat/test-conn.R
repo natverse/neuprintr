d0ab974bf6f8131a92c30677e9b4ba9cafdc609e
Gregory Jefferis <jefferis@gmail.com>
2021-08-05 23:59:54 +0100

    sty: skip should be used inside a test_that block

--- tests/testthat/test-conn.R
+++ tests/testthat/test-conn.R
@@ -13,8 +13,8 @@ test_that("neuprint_login works", {
   expect_error(neuprint_login(server="https://neuprint.janelia.org", dataset='rhubarb'))
 })
 
-skip("don't test bad connection specs")
 test_that("neuprint_connection works", {
+  skip("don't test bad connection specs")
   expect_warning(expect_is(
     neuprint_login(server = "neuprint.janelia.org",
                    dataset = 'hemibrain:v1.0.1',
