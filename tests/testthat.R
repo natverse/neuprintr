1bd635021c61dcf4fe3113735c7a07912cf0074f
Gregory Jefferis <jefferis@gmail.com>
2021-08-05 23:56:00 +0100

    specify server+dataset for tests
    * also better construction of test for server setting in tests entry point
    
    https://github.com/natverse/neuprintr/pull/144/checks?check_run_id=3256130789#step:9:226

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,8 @@ library(testthat)
 library(neuprintr)
 
 conn=try(neuprint_login())
-if(isTRUE(conn$server=="https://neuprint.janelia.org")) {
+if(!inherits(conn, 'try-error') &&
+   isTRUE(conn$server=="https://neuprint.janelia.org")) {
   Sys.setenv(SKIP_NP_SERVER_TESTS="FALSE")
 } else {
   Sys.setenv(SKIP_NP_SERVER_TESTS="TRUE")
